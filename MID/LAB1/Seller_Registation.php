08dc380b5d3cbd5362eeabb896c719f124606e29
DebnathKaushik <debnathkaushik49@gmail.com>
2025-04-13 12:11:46 +0600

    Form validation

--- MID/LAB1/Seller_Registation.php
+++ MID/LAB1/Seller_Registation.php
@@ -14,7 +14,7 @@
 <table>
     <tr>
        <td>
-        <form method="post" action="/Project/web/MID/LAB1/home.php">
+        <form method="post" action="/Project/web/MID/LAB1/home.php" onsubmit="return validate()">
             
         <div class="box1">
             <fieldset>
@@ -22,32 +22,32 @@
                     <table>
                         <tr>
                             <th align="left">Seller Name:</th>
-                            <td>
-                                <input type="text" name="seller_name" >
+                            <td class="field-group">
+                                <input type="text" name="seller_name" id="S_name" placeholder="Kaushik"> <span class="error" style="color:red" id="one"></span>
                             </td> 
                         </tr>
                         <tr>
                             <th align="left">Email Address:</th>
                             <td>
-                                <input type="text" name="email" >
+                                <input type="text" name="email" id="email" placeholder="user@gmail.com"> <span style="color:red" id="two"></span>
                             </td>
                         </tr>
                         <tr>
                             <th align="left">Phone Number:</th>
                             <td>
-                                <input type="text" name="phone_number" >
+                                <input type="text" name="phone_number" id="PH_num" placeholder="019********"> <span style="color:red" id="three"></span>
                             </td>
                         </tr>
                         <tr>
                             <th align="left">Password:</th>
                             <td>
-                                <input type="password" name="password" >
+                                <input type="password" name="password" id="Pass" placeholder="abc1$&"> <span style="color:red" id="four"></span>
                             </td>
                         </tr>
                         <tr>
-                            <th align="left">Confirm Password:</th>
+                            <th align="left">Confirm Password:</th> 
                             <td>
-                                <input type="password" name="confirm_password" >
+                                <input type="password" name="confirm_password" id="Conf_pass" placeholder="abc1$&"> <span style="color:red" id="five"></span>
                             </td>
                         </tr>
                     </table>
@@ -62,25 +62,28 @@
                     <tr>
                         <th align="left">Company Name:</th>
                         <td>
-                            <input type="text" name="company_name" >
+                            <input type="text" name="company_name" id="Com_name" placeholder="SuperBlock or tigerIT"> <span style="color:red" id="six"></span>
                         </td>
                     </tr>
                     <tr>
                         <th align="left">Company Address:</th>
                         <td>
-                            <input type="text" name="company_address" >
+                            <input type="text" name="company_address" id="Com_add" placeholder="Kuril,Dhaka,1210"> <span style="color:red" id="seven"></span>
                         </td>
                     </tr>
                     <tr>
                         <th align="left">Type of Service:</th>
                         <td>
-                            <select id="service_category" name="service_category">
-                                <option name="Personal">Personal</option>
-                                <option name="Group Tour">Group Tour</option>
-                                <option name="Family">Family</option>
-                                <option name="School">School</option>
-                                <option name="Corporate">Corporate</option>
-                            </select>
+                                <select id="Serv_Cat" name="service_category">
+                                    <option value="">-- Select Service --</option>
+                                    <option value="Personal">Personal</option>
+                                    <option value="Group Tour">Group Tour</option>
+                                    <option value="Family">Family</option>
+                                    <option value="School">School</option>
+                                    <option value="Corporate">Corporate</option>
+                                </select>
+                            <span style="color:red" id="eight"></span>
+
                         </td>
                     </tr>
                 
@@ -96,36 +99,40 @@
                     <tr>
                         <th align="left">Bus Name:</th>
                         <td>
-                            <input type="text" name="bus_name" >
+                            <input type="text" name="bus_name" id="Bus_name" placeholder="Bus Poribhohon"> <span style="color:red" id="nine"></span>
                         </td>
                     </tr>
             
                     <tr>
                         <th align="left">License Number:</th>
                         <td>
-                            <input type="text" name="License_number" >
+                            <input type="text" name="License_number" id="Lic_num" placeholder="9645878521"> <span style="color:red" id="ten"></span>
                         </td>
                     </tr>
                     <tr>
                         <th align="left">Upload Bus Documents:</th>
                         <td>
-                            <input type="file" name="business_documents" multiple>
+                            <input type="file" name="business_documents" id="busi_doc" multiple >
                         </td>
                     </tr>
                     <tr>
                         <th align="left">Type of Buses:</th>
                         <td>
-                            <input type="radio" name="bus_type" value='AC'>AC
+                            <input type="radio" name="bus_type" value='AC'>AC 
                             <input type="radio" name="bus_type" value='Non AC'>Non AC
                             <input type="radio" name="bus_type" value='Sleeper'>Sleeper
-                            <input type="radio" name="bus_type" value='Luxury'>Luxury
+                            <input type="radio" name="bus_type" value='Luxury'>Luxury 
+                            <br>
+                            <span style="color:red" id="twelve"></span>
                         </td>
+                        
+                       
                     </tr>
                     <tr>
-                        <th align="left">Registation Date:</th>
+                        <!-- <th align="left">Registation Date:</th>
                         <td>
                             <input type="date" name="registation_date">
-                        </td>
+                        </td> -->
                     </tr>
                 </table>
             </fieldset>
@@ -135,15 +142,17 @@
             <tr>
                 <th align="left">Agree to Terms & Conditions:</th>
                 <td>
-                    <input type="checkbox" name="terms" required> I agree to the <a href="">Terms & Conditions</a>
+                    <input type="checkbox" name="terms" id="Terms"> I agree to the <a href="">Terms & Conditions</a> <br>
+                    <span style="color:red" id="thirteen"></span>
                 </td>
+                
             </tr>
         </table>
         <br>
         <table>
             <tr>
                 <td align="left">
-                    <input type="submit" class="submit" name="submit" value="Register">  <!--Here, value="Register" sets the text inside the button-->
+                    <input type="submit" class="submit" name="submit" value="Register" id="submit_btn"> <span  id="fourtheen"></span> <!--Here, value="Register" sets the text inside the button-->
                 </td>
             </tr>
         </table>  
@@ -162,12 +171,14 @@
 
 
   
-    <td>
+    <!-- <td>
     <img src="images/bus.avif"  width="1024" height="600">
-    </td>
+    </td> -->
     
   </tr>
 </table>
+
+<script src="../LAB4/validation.js"></script>
 
 </body>
 </html>
---
+++ MID/LAB4/validation.js
@@ -0,0 +1,172 @@
+// Seller Name
+function SellerNameValidaton(){
+    const sellerName = document.getElementById("S_name").value
+    if(sellerName == ""){
+        document.getElementById("one").innerHTML = "Name is required !"
+        //alert("Name is required ")
+        return false
+    }else if(sellerName.length <= 3){
+        document.getElementById("one").innerHTML = "Name must be more than 3 charecters!"
+        return false
+    }else{
+        return true
+    }
+}
+
+// Seller Email
+function EmailValidaton(){
+    const email = document.getElementById("email").value
+    if(email == ""){
+        document.getElementById("two").innerHTML = "Email is required!"
+        return false
+    }else if(!email.includes("@") || !email.includes(".com")){             // false || false = false 
+        document.getElementById("two").innerHTML = "Enter Valid Email !"
+        return false
+    }else{
+        return true
+    }
+}
+
+// Phone Number
+function PhoneValidaton(){
+    const PhoneNumber = document.getElementById("PH_num").value
+    let patt = /^01\d{9}$/
+    if(PhoneNumber == ""){
+        document.getElementById("three").innerHTML = "Phone Number is required!"
+        return false
+    }else if(!patt.test(PhoneNumber) ){      // pattern milse -> true -> ! true = false         
+        document.getElementById("three").innerHTML = "Enter 11 digits Phone Number !" // false -> do not enter this line
+        return false
+    }else{
+        return true
+    }
+}
+
+
+// Password
+function PasswordValidaton(){
+    const Password = document.getElementById("Pass").value
+    const patt = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).{7,}$/;  // regular expression (Letter , Digits, Special charecter and atleast 7 )
+
+    if(Password == ""){
+        document.getElementById("four").innerHTML = "Password is required!"
+        return false
+    }else if(Password.length <= 6){
+         document.getElementById("four").innerHTML = "Password should be more than 6 charecters!"
+         return false
+    }else if(!patt.test(Password)){            
+        document.getElementById("four").innerHTML = "Password should digits,letter and special charecters!"
+       return false
+    }else{
+        return true
+    }
+}
+
+// Confirm Password
+function Conf_PasswordValidaton(){
+    const conf_password = document.getElementById("Conf_pass").value
+    const Password = document.getElementById("Pass").value
+
+    if(conf_password == "" ){
+        document.getElementById("five").innerHTML = "Confirm Password is required!"
+        return false
+    }else if(conf_password !== Password){
+        document.getElementById("five").innerHTML = "Confirm Password is not match!"
+        return false
+    }else{
+        return true
+    }
+}
+
+// Bus name
+function Bus_validation() {
+    const bus_name = document.getElementById("Bus_name").value;
+    const patt = /^[A-Za-z]{3,}$/
+    if (bus_name === "") {
+        document.getElementById("nine").innerHTML = "Bus name is required!";
+        return false;
+    }else if(!patt.test(bus_name)){
+        document.getElementById("nine").innerHTML = "Bus name should only letter and  more than 3 char!";
+        return false;
+    }else{
+        return true;
+    }
+}
+
+// License
+function License_validation() {
+    const License_name = document.getElementById("Lic_num").value;
+    const patt = /^\d{5,}$/    // only digits and at least 5 
+    if (License_name === "") {
+        document.getElementById("ten").innerHTML = "License is required!";
+        return false;
+    }else if(!patt.test(License_name)){
+        document.getElementById("ten").innerHTML = "License should be digits and at least 5 digits!";
+        return false;
+    }else{
+        return true;
+    }
+}
+
+// Services 
+function service_validation() {
+    const service_name = document.getElementById("Serv_Cat").value;
+    if (service_name === "") {
+        document.getElementById("eight").innerHTML = "Services is not selected";
+        return false;
+    }else{
+        return true;
+    }
+}
+
+// Terms and Condition
+function terms_validation() {
+        const termsCheckbox = document.getElementById("Terms");
+
+        if (!termsCheckbox.checked) {
+            document.getElementById("thirteen").innerText = "You must agree to the terms and conditions.";
+            return false
+        } else {
+            return true
+        }   
+}
+
+
+// radio button
+function busTypeValidation() {
+    let Bool = false;
+    const busTypes = document.getElementsByName("bus_type");
+
+    for (let i = 0; i < busTypes.length; i++) {
+        if (busTypes[i].checked) {
+            Bool = true;
+            break;
+        }
+    }
+    if (!Bool) {
+        document.getElementById("twelve").innerText = "Please select a type of bus.";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+
+// Validation Function
+function validate() {
+   return  (SellerNameValidaton() && 
+            EmailValidaton() && 
+            PhoneValidaton() && 
+            PasswordValidaton() && 
+            Conf_PasswordValidaton() &&
+            service_validation() &&
+            Bus_validation() &&
+            License_validation() &&
+            busTypeValidation() &&
+            terms_validation()
+            
+        )
+           
+}
+
