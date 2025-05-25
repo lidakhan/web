59d812b3b4dba4f8785e6c7ec38a713ff3cba935
DebnathKaushik <debnathkaushik49@gmail.com>
2025-05-25 09:54:14 +0600

    update delete part

--- FINAL/view/admin.php
+++ FINAL/view/admin.php
@@ -1,5 +1,6 @@
 <?php
-include "../control/adminlogincontrol.php"; // your session and access control
+//include "../control/adminlogincontrol.php"; 
+include "../control/admin_control.php"           // session and access control
 ?>
 
 <!DOCTYPE html>
@@ -7,6 +8,7 @@ include "../control/adminlogincontrol.php"; // your session and access control
 <head>
     <title>Admin Dashboard</title>
     <link rel="stylesheet"  href="admin_style.css">
+    <link rel="stylesheet"  href="logout.css">
 </head>
 <body>
 
@@ -27,21 +29,26 @@ include "../control/adminlogincontrol.php"; // your session and access control
     $sql = "SELECT * FROM users";
     $result = mysqli_query($conn, $sql);
 
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row['id'] . "</td>";
-        echo "<td>" . htmlspecialchars($row['seller_name']) . "</td>";
-        echo "<td>" . htmlspecialchars($row['email']) . "</td>";
-        echo "<td>
-                <a href='update_user.php?id=" . $row['id'] . "'>Update</a> |
-                <a href='admin.php?delete_id=" . $row['id'] . "' onclick=\"return confirm('Delete this user?');\">Delete</a>
-              </td>";
-        echo "</tr>";
-    }
+   while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . htmlspecialchars($row['seller_name']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['email']) . "</td>";
+    echo "<td>
+            <a href='update_user.php?id=" . $row['id'] . "'>Update</a> |
+            <a href='admin.php?delete_id=" . $row['id'] . "' onclick='return confirm(\"Delete this user?\");'>Delete</a>
+          </td>";
+    echo "</tr>";
+}
+
 
     closeCon($conn);
     ?>
 </table>
+
+    <div >
+        <button class="three" type="button" onclick="location.href='../control/logout.php'" >logout
+    </div>
 
 </body>
 </html>
